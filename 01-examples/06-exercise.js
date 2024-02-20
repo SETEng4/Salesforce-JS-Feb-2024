6a36c773bfc8d3b774dd89ff578878607ad25e7d
Magesh Kuppan <tkmagesh77@gmail.com>
2024-02-21 00:06:39 +0530

    programming constructs - exercise - solution

--- 01-examples/06-exercise.js
+++ 01-examples/06-exercise.js
@@ -1,3 +1,20 @@
 /* 
 Write a program that will print the first 10 prime numbers between 3 and 100
 */
+var primeCount = 0;
+for (var no = 3; no <= 100; no++){
+    var isPrime = true;
+    if (primeCount >= 10){
+        break;
+    }
+    for (var i = 2; i <= (no/2); i++){
+        if (no % i == 0){
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime){
+        primeCount++
+        console.log('Prime No :', no)
+    }
+}
