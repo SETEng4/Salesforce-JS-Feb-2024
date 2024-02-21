e06775baffce7a643805ae643f02fb6e6b99f374
Magesh Kuppan <tkmagesh77@gmail.com>
2024-02-21 20:16:49 +0530

    for...in

--- 01-examples/17-objects.js
+++ 01-examples/17-objects.js
@@ -38,3 +38,12 @@ var obj = {
 obj.whoAmI()
 obj.name = 'Dennis'
 obj.whoAmI()
+
+// Iterating through the attributes of an object
+// for...in
+var product = { id : 100, name : 'Pen', cost : 10, category : 'stationary'}
+for(var key in product){
+    console.log(key)
+}
+
+// print the name & value of all the attributes in the product object
