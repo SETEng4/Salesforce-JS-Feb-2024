83d058513780eb3dc51f7db1ef162c8fdeaa97cf
Magesh Kuppan <tkmagesh77@gmail.com>
2024-02-21 20:44:40 +0530

    exercise

---
+++ 18-exercise.js
@@ -0,0 +1,25 @@
+var products = [
+    { id: 6, name: 'Pen', cost: 50, units: 20, category: 'stationary' },
+    { id: 9, name: 'Ten', cost: 70, units: 70, category: 'stationary' },
+    { id: 3, name: 'Len', cost: 60, units: 60, category: 'grocery' },
+    { id: 5, name: 'Zen', cost: 30, units: 30, category: 'grocery' },
+    { id: 1, name: 'Ken', cost: 20, units: 80, category: 'utencil' },
+    { id: 7, name: 'Mouse', cost: 100, units: 20, category: 'electronics' }
+];
+
+// Solve the below without using loops
+
+// print the name & cost of all the products
+
+// print only the stationary products
+
+// create a new array of products (discountedProducts) with the cost applied with 10% discount
+
+// find the overall products value (sum of (units * cost))
+
+// find the costliest product
+
+// find the cheapest product
+
+// group the products by category
+
